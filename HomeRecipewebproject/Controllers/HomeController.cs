c8e197c601df354acf95a1c3a324f933cb8d2450
haseeb <haseebmhaseeb547@gmail.com>
2024-06-05 13:20:27 +0500

    Add project files.

---
+++ HomeRecipewebproject/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeRecipewebproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Recipes()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeRecipewebproject/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeRecipewebproject
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
